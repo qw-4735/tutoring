60bdb6265ef0f3609efc6266c138167dec814006
00HS <98587833+00HS@users.noreply.github.com>
2023-03-07 15:24:22 +0900

    Update GGJ_1.py

--- day2/GGJ_1.py
+++ day2/GGJ_1.py
@@ -4,12 +4,17 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 torch.manual_seed(1)
+#GPU를 통해 만들어내는 결과들에 randomness 를 통제
 
 #%%
 # 단순 선형 회귀
 
+#훈련데이터인 x_train과 y_train을 선언
 x_train = torch.FloatTensor([[1], [2], [3]])
+#(3×1)의 크기를 가지는 2차원 텐서(행렬) 생성
 y_train = torch.FloatTensor([[2], [4], [6]])
+#(3×1)의 크기를 가지는 2차원 텐서(행렬) 생성
+
 
 W = torch.zeros(1, requires_grad=True)
 b = torch.zeros(1, requires_grad=True)

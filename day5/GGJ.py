551c374e661fca657ee179cabbbbfdaf283d526b
qw-4735 <78010864+qw-4735@users.noreply.github.com>
2023-03-23 20:10:29 +0900

    Add files via upload

---
+++ day5/GGJ.py
@@ -0,0 +1,77 @@
+import torch
+from torch import nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions.multivariate_normal import MultivariateNormal
+
+import numpy as np
+
+from itertools import repeat
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
+else:
+    device = torch.device('cpu')
+
+
+# data generation
+seed = 0
+torch.manual_seed(seed)
+
+mean_vec = list(repeat(1, 50)) + list(repeat(3, 50))
+mean = torch.Tensor([mean_vec, list(reversed(mean_vec))])
+cov1 = torch.eye(100)
+cov = torch.stack([cov1, cov1], 0)
+distrib = MultivariateNormal(loc=mean, covariance_matrix=cov)
+
+x = distrib.rsample().T
+beta = torch.rand(2).uniform_(-1, 1)
+y = torch.bernoulli(torch.sigmoid(x @ beta)).to(torch.float32)
+# y = torch.tensor(list(repeat(1., 50)) + list(repeat(0., 50)))
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = seed)
+x_train = x_train.to(device)
+x_test = x_test.to(device)
+y_train = y_train.to(device)
+y_test = y_test.to(device)
+
+
+
+
+# 1. Logit layer 작성
+
+
+
+
+
+
+
+
+
+
+# 2. cross entropy 함수 작성
+
+
+
+
+
+
+# 3. Logit layer를 이용한 LogisticRegression 모형 작성
+
+
+
+
+
+
+
+
+# 4. 학습 코드 작성

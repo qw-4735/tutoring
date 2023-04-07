20cdbdfde86f2aa73c0cda6179e29c27c9508a3b
qw-4735 <78010864+qw-4735@users.noreply.github.com>
2023-04-07 17:49:29 +0900

    Add files via upload

---
+++ day7/GGJ.py
@@ -0,0 +1,3 @@
+# 1. https://towardsdatascience.com/building-rnn-lstm-and-gru-for-time-series-using-pytorch-a46e5b094e7b 
+# data : https://www.kaggle.com/datasets/robikscube/hourly-energy-consumption
+# fitting the RNN model constructed in day6 (모델 구현할 때 line by line으로 차원 계산해보고 주석 달 것)

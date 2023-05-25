d1f04757befc7f10875a22edfe807f4aa8cb5dfd
qw-4735 <78010864+qw-4735@users.noreply.github.com>
2023-05-25 17:33:07 +0900

    Delete day14 directory

--- day14/GGJ.py
+++
@@ -1,6 +0,0 @@
-# Transformer
-# https://doheon.github.io/%EC%BD%94%EB%93%9C%EA%B5%AC%ED%98%84/time-series/ci-4.transformer-post/
-
-# 참고할 만한 자료
-# 1) https://velog.io/@uvictoli/Module-7.-%E3%80%8E%EC%8B%9C%EA%B3%84%EC%97%B4-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EB%B0%8F-AI-%EB%AA%A8%EB%8D%B8-%EC%84%B1%EB%8A%A5-%EC%B5%9C%EC%A0%81%ED%99%94%E3%80%8F-3.-%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8-%EA%B8%B0%EB%B0%98%EC%9D%98-%EC%8B%9C%EA%B3%84%EC%97%B4-%EB%8D%B0%EC%9D%B4%ED%84%B0-%ED%9A%8C%EA%B7%80#time-series-transformer-tst
-# 2) https://inhovation97.tistory.com/71
